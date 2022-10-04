8bcbcb2432d95f5c921e0abe3d28c25a74e7d622
DiCode77 <Dimazamshaq@icloud.com>
2022-10-04 20:46:57 +0300

    Added allocation of allocated space, line output.

--- String/Str.cpp
+++ String/Str.cpp
@@ -6,3 +6,41 @@
 //
 
 #include "Str.hpp"
+
+void Str::copy_variable(char *str, const char *in_str, const ulong &len){
+    if (!str || !in_str || !len){
+        return;
+    }
+    else{
+        for (ulong i = 0; i < len; i++){
+            str[i] = in_str[i];
+        }
+    }
+}
+
+bool Str::maxsize(const ulong &len){
+    return this->capacit_y > len;
+}
+
+bool Str::minsize(const ulong &len){
+    return (len + STR_CAPACITY) < this->capacit_y;
+}
+
+void Str::reall_size(const ulong &len){
+    if (!maxsize(len)){
+        this->capacit_y += (len - this->capacit_y) + STR_CAPACITY;
+    }
+    else if (minsize(len)){
+        this->capacit_y = len + STR_CAPACITY;
+    }
+
+}
+
+void Str::new_str_size(const char *oth_str, ulong &len){
+    if (oth_str != nullptr || len){
+        char *new_str = new char[this->capacit_y +1]{};
+        copy_variable(new_str, oth_str, len);
+        delete [] this->str;
+        this->str = new_str;
+    }
+}
--- String/Str.hpp
+++ String/Str.hpp
@@ -4,12 +4,76 @@
 //
 //  Created by DiCode77.
 //
+#include <iostream>
+
+#define STR_VERSION "0.0.1"
+#define STR_CAPACITY 22
+typedef unsigned long ulong;
 
 class Str{
     char *str;
+    ulong lenght;
+    ulong capacit_y;
     
 public:
     Str(){
         this->str = nullptr;
+        this->lenght = 0;
+        this->capacit_y = STR_CAPACITY;
+    }
+    
+    Str(const char *in_str) : Str(){
+        if (in_str == nullptr){
+            return;
+        }
+        else{
+            while (in_str[this->lenght] != '\0'){
+                this->lenght++;
+            }
+            this->str = new char[this->lenght +1]{};
+            copy_variable(this->str, in_str, this->lenght +1);
+            reall_size(this->lenght);
+        }
+    }
+    
+    
+public:
+    
+    Str &operator= (const Str &oth){
+        if (oth.str != nullptr){
+            this->lenght = oth.lenght;
+            reall_size(this->lenght);
+            new_str_size(oth.str, this->lenght);
+        }
+        return *this;
+    }
+    
+    
+    friend std::ostream& operator << (std::ostream &out, const Str &other){
+        return (out << other.str);
+    }
+    
+    ulong size(){
+        return this->lenght;
+    }
+    
+    ulong capacity(){
+        return this->capacit_y;
+    }
+    
+    const char *c_str(){
+        return this->str;
+    }
+    
+private:
+    bool maxsize(const ulong&);
+    bool minsize(const ulong&);
+    void reall_size(const ulong&);
+    void new_str_size(const char*, ulong&);
+    void copy_variable(char*, const char*, const ulong&);
+    
+public:
+    ~Str(){
+        delete [] this->str;
     }
 };
