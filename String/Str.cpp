4bf648ca4691ba5640dc09fed7bb01bef6daf85e
DiCode77 <Dimazamshaq@icloud.com>
2022-10-05 16:09:04 +0300

    Added the ability to concatenate a string, bugs fixed

--- String/Str.cpp
+++ String/Str.cpp
@@ -7,6 +7,12 @@
 
 #include "Str.hpp"
 
+void Str::new_str(const ulong &size){
+    if (this->str == nullptr){
+        this->str = new char[size]{};
+    }
+}
+
 void Str::copy_variable(char *str, const char *in_str, const ulong &len){
     if (!str || !in_str || !len){
         return;
@@ -14,6 +20,17 @@ void Str::copy_variable(char *str, const char *in_str, const ulong &len){
     else{
         for (ulong i = 0; i < len; i++){
             str[i] = in_str[i];
+        }
+    }
+}
+
+void Str::copy_variable_position(const ulong &pos, char *str, const char *in_str, const ulong &len){
+    if (!str || !in_str || !len){
+        return;
+    }
+    else{
+        for (ulong i = pos; i < len; i++){
+            str[i] = in_str[i - this->lenght];
         }
     }
 }
@@ -36,11 +53,40 @@ void Str::reall_size(const ulong &len){
 
 }
 
-void Str::new_str_size(const char *oth_str, ulong &len){
-    if (oth_str != nullptr || len){
+void Str::new_str_size(const char *oth_str, const ulong &len){
+    if (oth_str != nullptr){
         char *new_str = new char[this->capacit_y +1]{};
-        copy_variable(new_str, oth_str, len);
+        copy_variable(new_str, oth_str, len +1);
         delete [] this->str;
         this->str = new_str;
     }
+    else{
+        this->str = new char[this->capacit_y +1]{};
+        copy_variable(this->str, oth_str, len +1);
+    }
+}
+
+Str &Str::operator= (const Str &oth){
+    if (oth.str != nullptr){
+        this->lenght = oth.lenght;
+        reall_size(this->lenght);
+        new_str_size(oth.str, this->lenght);
+    }
+    return *this;
+}
+
+Str &Str::operator+= (const Str &oth){
+    reall_size(this->lenght + oth.lenght);
+    new_str_size(this->str, this->lenght);
+    copy_variable_position(this->lenght, this->str, oth.str, (this->lenght + oth.lenght) +1);
+    this->lenght = this->lenght + oth.lenght;
+    return *this;
+}
+
+Str &Str::operator+ (const Str &oth){
+    reall_size(this->lenght + oth.lenght);
+    new_str_size(this->str, this->lenght);
+    copy_variable_position(this->lenght, this->str, oth.str, (this->lenght + oth.lenght) +1);
+    this->lenght = this->lenght + oth.lenght;
+    return *this;
 }
--- String/Str.hpp
+++ String/Str.hpp
@@ -22,6 +22,13 @@ public:
         this->capacit_y = STR_CAPACITY;
     }
     
+    Str(Str &str) : Str(){
+        this->lenght = str.lenght;
+        reall_size(this->lenght);
+        new_str(this->capacit_y +1);
+        copy_variable(this->str, str.str, this->lenght +1);
+    }
+    
     Str(const char *in_str) : Str(){
         if (in_str == nullptr){
             return;
@@ -30,24 +37,23 @@ public:
             while (in_str[this->lenght] != '\0'){
                 this->lenght++;
             }
-            this->str = new char[this->lenght +1]{};
-            copy_variable(this->str, in_str, this->lenght +1);
             reall_size(this->lenght);
+            new_str(this->capacit_y +1);
+            copy_variable(this->str, in_str, this->lenght +1);
         }
     }
     
     
 public:
+    Str &operator= (const Str &oth);
     
-    Str &operator= (const Str &oth){
-        if (oth.str != nullptr){
-            this->lenght = oth.lenght;
-            reall_size(this->lenght);
-            new_str_size(oth.str, this->lenght);
-        }
-        return *this;
+    Str &operator+= (const Str &oth);
+    
+    Str &operator+ (const Str &oth);
+    
+    char &operator[] (const ulong &in){
+        return this->str[in];
     }
-    
     
     friend std::ostream& operator << (std::ostream &out, const Str &other){
         return (out << other.str);
@@ -66,11 +72,13 @@ public:
     }
     
 private:
+    void new_str(const ulong&);
     bool maxsize(const ulong&);
     bool minsize(const ulong&);
     void reall_size(const ulong&);
-    void new_str_size(const char*, ulong&);
+    void new_str_size(const char*, const ulong&);
     void copy_variable(char*, const char*, const ulong&);
+    void copy_variable_position(const ulong&, char*, const char*, const ulong&);
     
 public:
     ~Str(){
